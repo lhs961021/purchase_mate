348011607fbf14268798dce24d54776cccea2d9a
tkdwns414 <sangjune2000@naver.com>
2021-08-06 22:56:29 +0900

    Add edit,delete(community)

--- community/urls.py
+++ community/urls.py
@@ -4,7 +4,11 @@ from .views import *
 app_name = "community"
 
 urlpatterns = [
+    path("", community, name="community"),
+    path("<str:id>", detail, name="detail"),
     path("new/", new, name="new"),
     path("create/", create, name="create"),
-    path("<str:id>", detail, name="detail"),
+    path("edit/<str:id>", edit, name="edit"),
+    path("update/<str:id>", update, name="update"),
+    path("delete/<str:id>", delete, name="delete"),
 ]
