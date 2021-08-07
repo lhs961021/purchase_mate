53eb5666fde5943a568d392ffa35d1616854067f
Siyun Kim <sharon1638@naver.com>
2021-08-07 09:47:49 +0900

    sort_and_navbar

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import date
 
 # Create your models here.
+
+class Post(models.Model):
+    id=models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    # writer=models.ForeignKey(User,on_delete=models.CASCADE)
+    pub_date=models.DateTimeField(auto_now=True) #수정한 후에 업로드시간 및 날짜 변하지 않게 다시 수정할 것!
+    deadline=models.DateField(auto_now=False)
+    quantity=models.IntegerField(default=0)
+    price=models.IntegerField(default=0)
+    people=models.IntegerField(default=0, null=True)
+    category=models.CharField(max_length=20)
+    image=models.ImageField(upload_to="blog/",blank=True,null=True )
+    chat=models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.title
+
--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,24 @@ from .views import *
 app_name = "posts"
 
 urlpatterns = [
-    path("", shop, name="shop"),
-    path("shop_detail/", shop_detail, name="shop_detail"),
+    path("",shop, name="shop"),
+    path("shop_detail/",shop_detail, name="shop_detail"),
+    #정렬부분
+    path("recent_postlist/",recent_postlist,name="recent_postlist"),
+    path("all_postlist/",all_postlist,name="all_postlist"),
+    path("deadline_postlist/",deadline_postlist,name="deadline_postlist"),
+    path("distance_postlist/",distance_postlist,name="distance_postlist"),
+    path("price_postlist/",price_postlist,name="price_postlist"),
+    #CRUD부분
+    path("makepost/",makepost,name="makepost"),
+    path("<str:id>",detail,name="detail"),
+    path("create/",create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>',update, name="update"),
+    path('delete/<str:id>', delete, name="delete"),
+    #카테고리부분
+    path('category/food/',food,name="food"),
+    path('category/daily_necessity/',daily_necessity,name="daily_necessity"),
+    path('category/ott/',ott,name="ott"),
+    path('category/etc/',etc,name="etc"),
 ]
